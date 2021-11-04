0ecd68e29361fb5dc4ca3c6888793b823998269d
jflorido94 <58602366+jflorido94@users.noreply.github.com>
2021-11-04 19:02:52 +0100

    passport working 0.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,18 +31,6 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [AuthController::class, 'login']);
+Route::post('signup', [AuthController::class, 'signup']);
+
+Route::group([
+    'middleware' => 'auth:api'
+], function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'user']);
 });
+
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
