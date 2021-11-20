2f67b21f27a1c4f98ee4f67b0a96f2bb07eeb337
jflorido94 <58602366+jflorido94@users.noreply.github.com>
2021-11-20 19:10:45 +0100

    try fix cors issues 0.3

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware(['cors'])->group(function () {
 
     //---- Login---
     Route::post('login', [AuthController::class, 'login']);
@@ -147,5 +146,3 @@ use Illuminate\Support\Facades\Route;
         Route::delete('/{message}', [MessageController::class, 'destroy'])->middleware('auth:api');
     });
 
-
-// });
