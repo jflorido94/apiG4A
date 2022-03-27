3d1808daa1db0eecf8c3172526f7ddc6cbe98ecd
jflorido94 <hbranky22@hotmail.com>
2022-03-27 23:23:40 +0200

    cors Entrega

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
